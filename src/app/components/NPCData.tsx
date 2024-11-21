4275085beb5c0022b29cc155c86bd97249045334
Marcos-Antoni <marcos7barrera@gmail.com>
2024-11-21 11:40:47 -0600

    arreglando el build de la app

--- src/app/components/NPCData.tsx
+++ src/app/components/NPCData.tsx
@@ -1,23 +1,25 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ParentGenerator from "./Factory/ParentGenerator";
 import { tablets } from "../utils/dataTablets";
 
 const NPCData = () => {
   return (
     <div className="grid md:grid-cols-2 gap-6">
-      <ParentGenerator dataTable={tablets.tipoNPC} name="Bacico" />
-      <ParentGenerator
-        dataTable={tablets.motivaciónNPC}
-        name="Motivacion"
-      />
-      <ParentGenerator
-        dataTable={tablets.personalidadNPC}
-        name="Personalidad"
-      />
-      <ParentGenerator
-        dataTable={tablets.aliniamientos}
-        name="Aliniamientos"
-      />
+      <Suspense fallback={<div>Loading...</div>}>
+        <ParentGenerator dataTable={tablets.tipoNPC} name="Bacico" />
+        <ParentGenerator
+          dataTable={tablets.motivaciónNPC}
+          name="Motivacion"
+        />
+        <ParentGenerator
+          dataTable={tablets.personalidadNPC}
+          name="Personalidad"
+        />
+        <ParentGenerator
+          dataTable={tablets.aliniamientos}
+          name="Aliniamientos"
+        />
+      </Suspense>
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
+import React, { Suspense } from "react";
 import NPCData from "./components/NPCData";
 import TextAreaComponent from "./components/TextareaComponent";
 import NPCGenerator from "./components/NPCGenerator";
 
-export default function App() {
+export default function Page() {
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100 py-[50px]">
       <div className="flex flex-col gap-[20px] w-[90%] max-w-[700px]">
         <h1 className="text-4xl font-bold mb-6">Generador de NPC</h1>
 
-        <TextAreaComponent />
+        <Suspense fallback={<div>Loading...</div>}>
+          <TextAreaComponent />
+        </Suspense>
+
         <NPCData />
         <NPCGenerator />
       </div>
